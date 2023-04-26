dcb31c32243f9a757b4120a33e3b9dcebc9fa2a0
oeyoews <jyao4783@gmail.com>
2023-04-26 23:36:55 +0800

    Automatic commit

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { View, Text } from "@tarojs/components";
 import { useLoad } from "@tarojs/taro";
 import Welcome from "../../components/Welcome";
-import Summary from "../../components/Summary";
 import "./index.css";
 import "windi.css";
 
@@ -12,11 +11,10 @@ export default function Index() {
   });
 
   return (
-    <View className="index bg-indigo-200 p-2 rounded  ">
-      Thihs is dmeo
-      <Text className="bg-red-200 rounded hover:bg-red-400 transition duration-200">Thi is index page(beta test)</Text>
-      <Summary number={1 + 2} />
-      <Welcome name="This is a demo, success" />
-    </View>
+    <>
+      <View className="index bg-indigo-200 p-2 rounded  ">
+        <Welcome name="Success" />
+      </View>
+    </>
   );
 }
