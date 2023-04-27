078d4e1b7694544fa04f624be94e22272552ceef
oeyoews <jyao4783@gmail.com>
2023-04-27 23:21:37 +0800

    feat: add articles map

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -10,6 +10,34 @@ const goTo = () => {
 	})
 }
 
+const articles = [
+	{
+		id: 1,
+		title: 'Taro 入门',
+		content: 'Taro 入门'
+	},
+	{
+		id: 2,
+		title: 'Taro 高级用法',
+		content: 'Taro 高级用法'
+	}
+]
+
+const Articles = () => {
+	return (
+		<View>
+			{
+				articles.map(item => (
+					<View key={item.title}>
+						<Text>{item.title}</Text>
+						<Text>{item.content}</Text>
+					</View>
+				))
+			}
+		</View>
+	)
+}
+
 const gravatarClasses = "rounded-full w-12 h-12 shadow-lg m-2"
 
 function Blog() {
@@ -19,6 +47,7 @@ function Blog() {
 				<Image src='https://q1.qlogo.cn/g?b=qq&nk=2956398608&s=100' className={gravatarClasses}></Image>
 			</View>
 			<Image src={img01} className={gravatarClasses} ></Image>
+			<Articles />
 			<View>Blog Page</View>
 			<Button onClick={goTo} className='inline rounded bg-red-200 p-2 m-2 cursor-pointer hover:underline'>Nav to home page</Button>
 		</>
