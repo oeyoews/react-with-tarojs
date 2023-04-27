929bddfbcceb0b5179dc85d2bbdb914f8095f5a5
oeyoews <jyao4783@gmail.com>
2023-04-27 23:00:46 +0800

    Automatic commit

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -10,10 +10,15 @@ const goTo = () => {
 	})
 }
 
+const gravatarClasses = "rounded-full w-12 h-12 shadow-lg m-2"
+
 function Blog() {
 	return (
 		<>
-			<Image src={img01} className='rounded-full w-12 h-12 shadow-lg' ></Image>
+			<View className='flex justify-center items-center hover:scale-105 duration-200'>
+				<Image src='https://q1.qlogo.cn/g?b=qq&nk=2956398608&s=100' className={gravatarClasses}></Image>
+			</View>
+			<Image src={img01} className={gravatarClasses} ></Image>
 			<View>Blog Page</View>
 			<Button onClick={goTo} className='inline rounded bg-red-200 p-2 m-2 cursor-pointer hover:underline'>Nav to home page</Button>
 		</>
