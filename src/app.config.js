bb9f6c155a02e30b2d9892948e4a72a3f210d330
oeyoews <jyao4783@gmail.com>
2023-04-28 08:55:46 +0800

    fix: disable cache ti prevent error

--- src/app.config.js
+++ src/app.config.js
@@ -1,5 +1,5 @@
 export default defineAppConfig({
-  pages: ["pages/index/index","pages/blog/index","pages/test-page/index", "pages/test/index",],
+  pages: ["pages/index/index", "pages/blog/index", "pages/test-page/index", "pages/test/index",],
   window: {
     backgroundTextStyle: "light",
     navigationBarBackgroundColor: "#fff",
