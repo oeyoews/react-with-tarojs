d63c0d9ae7d94ac3f3d59045de2eb55400800c0e
oeyoews <jyao4783@gmail.com>
2023-04-29 23:18:40 +0800

    feat: add snippets

---
+++ src/components/Gravatar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from '@tarojs/cli'
+
+const Gravatar = () => {
+	return (
+		<View>
+			<Text></Text>
+		</View>
+	);
+};
+
+export default Gravatar;
